aa1b9ea79f6a96bb907e2928d1929d37cd5316a1
Andras Rutkai <andras.rutkai@logmein.com>
2018-10-25 12:40:54 +0200

    Test framework

---
+++ tests/PreflopTest.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+class PreflopTest extends \PHPUnit\Framework\TestCase
+{
+    public function testTrue() {
+        $this->assertTrue(true);
+    }
+}
