80472dd9c3f1b7c5df92029cbfc83516513d76f1
kocsismate <kocsismate@woohoolabs.com>
2018-10-25 11:38:53 +0200

    Improved Preflop stategy

--- preflop.php
+++ preflop.php
@@ -4,20 +4,22 @@ require_once "common.php";
 
 class Preflop
 {
-    private static $percentage = [
+    private static $percentages = [
         "AA" => 8,
         "KK" => 8,
         "QQ" => 8,
         "JJ" => 7,
-        "JJ" => 7,
         "1010" => 7,
-        "99" => 7,
         "99" => 7,
         "88" => 6,
     ];
 
     public function calculate(array $state): int
     {
+        Common::getCardsInHand($state);
+
+
+
         return (int)$state['current_buy_in'];
     }
 }
