6b6c6a7ad8a9944507baea3715a40033fecd60ed
kocsismate <kocsismate@woohoolabs.com>
2018-10-25 11:31:07 +0200

    Added some percentages

--- preflop.php
+++ preflop.php
@@ -4,6 +4,18 @@ require_once "common.php";
 
 class Preflop
 {
+    private static $percentage = [
+        "AA" => 8,
+        "KK" => 8,
+        "QQ" => 8,
+        "JJ" => 7,
+        "JJ" => 7,
+        "1010" => 7,
+        "99" => 7,
+        "99" => 7,
+        "88" => 6,
+    ];
+
     public function calculate(array $state): int
     {
         return (int)$state['current_buy_in'];
