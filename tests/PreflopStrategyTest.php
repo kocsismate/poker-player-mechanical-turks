91a15982ff71a1a116d1465f56a3286850504934
Gabor Csurgo <gcsurgo90@gmail.com>
2018-10-25 14:41:39 +0200

    New TCs

--- tests/PreflopStrategyTest.php
+++ tests/PreflopStrategyTest.php
@@ -8,10 +8,25 @@ use Poker\PreflopStrategy;
 
 class PreflopStrategyTest extends TestCase
 {
-    public function testTrue()
+    public function testAcePair()
     {
         $preflop = new PreflopStrategy();
 
-        $this->assertEquals(120, $preflop->calculate(["A", "A"], true, 100, 20));
+        $this->assertEquals(100, $preflop->calculate(["A", "A"], true, 100, 20));
+    }
+
+
+    public function testPairOfTwo()
+    {
+        $preflop = new PreflopStrategy();
+
+        $this->assertEquals(0, $preflop->calculate(["2", "2"], false, 100, 20));
+    }
+
+    public function testTwoSeven()
+    {
+        $preflop = new PreflopStrategy();
+
+        $this->assertEquals(0, $preflop->calculate(["2", "7"], false, 100, 20));
     }
 }
