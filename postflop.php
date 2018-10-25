d17d555047026e8c64a8cbc83f69091b6c2e8b12
Andras Rutkai <andras.rutkai@logmein.com>
2018-10-25 11:42:04 +0200

    Function skeletons

--- postflop.php
+++ postflop.php
@@ -9,8 +9,59 @@ class Postflop
         return (int)$state['current_buy_in'];
     }
 
-    private function getHandValue(): int
+    private function getHandValue($state): int
     {
 
     }
+
+    private function getAllCards($state): array
+    {
+        return Common::getCardsInHand($state) + $this->getCommonCards($state);
+    }
+
+    private function getCommonCards(array $state): array
+    {
+        return $state['community_cards'];
+    }
+
+    private function hasStraightFlush(array $cards): bool
+    {
+
+    }
+
+    private function hasFourOfAKind(array $cards): bool
+    {
+
+    }
+
+    private function hasFull(array $cards): bool
+    {
+
+    }
+
+    private function hasFlush(array $cards): bool
+    {
+
+    }
+
+    private function hasStraight(array $cards): bool
+    {
+
+    }
+
+    private function hasThreeOfAKind(array $cards): bool
+    {
+
+    }
+
+    private function hasTwoPairs(array $cards): bool
+    {
+
+    }
+
+    private function hasPair(array $cards): bool
+    {
+
+    }
+
 }
