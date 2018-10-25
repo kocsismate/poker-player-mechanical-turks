d8a38c995388dcc191f11b87be5c56907783d69e
kocsismate <kocsismate@woohoolabs.com>
2018-10-25 14:47:09 +0200

    Fix preflop tests

--- tests/PreflopStrategyTest.php
+++ tests/PreflopStrategyTest.php
@@ -12,20 +12,20 @@ class PreflopStrategyTest extends TestCase
     {
         $preflop = new PreflopStrategy();
 
-        $this->assertEquals(120, $preflop->calculate(["A", "A"], true, 100, 20));
+        $this->assertEquals(100, $preflop->calculate(["A", "A"], false, true, 100, 20));
     }
 
     public function testPairOfTwo()
     {
         $preflop = new PreflopStrategy();
 
-        $this->assertEquals(0, $preflop->calculate(["2", "2"], false, 100, 20));
+        $this->assertEquals(100, $preflop->calculate(["2", "2"], false, false, 100, 20));
     }
 
     public function testTwoSeven()
     {
         $preflop = new PreflopStrategy();
 
-        $this->assertEquals(0, $preflop->calculate(["2", "7"], false, 100, 20));
+        $this->assertEquals(0, $preflop->calculate(["2", "7"], false, false, 100, 20));
     }
 }
